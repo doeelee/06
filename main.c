d65b3a021359e9ac0df51221acdc82278154b398
doelele <doelele@users.noreply.github.com>
2020-10-08 15:51:36 +0900

    06-2

--- main.c
+++ main.c
@@ -4,19 +4,31 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-int starprint()
-	{
+int sumTwo(int a, int b)
+{
+	return (a+b);
+}
+    
+int square(int n)
+{
+	return (n*n);
+}
 
-	int i;
-	for(i = 0; i<10; i++)
-	   printf("*");
-	}
-
-	int main(int argc, char *argv[]) {
-		
-		starprint();
-		starprint();
-		starprint();   
+int get_max(int x, int y)
+{
+	if (x > y)
+	   return x;
+    else
+       return y;
+}  
+ 
+int main(int argc, char *argv[]) {
+	
+	printf("sumTwo : %i\n", sumTwo(1,2));
+	printf("square : % i\n", square(5));
+	printf("get_max : %i\n", get_max(10, 30));
 	
 	return 0;
-}
+}    
+    
+    
